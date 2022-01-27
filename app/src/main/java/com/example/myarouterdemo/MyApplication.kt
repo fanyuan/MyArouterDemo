50d9995c54549431bc655da0b020a645fffd166b
fly <lanseyoukong638@163.com>
2022-01-27 13:52:07 +0800

    判断语句更改

--- app/src/main/java/com/example/myarouterdemo/MyApplication.kt
+++ app/src/main/java/com/example/myarouterdemo/MyApplication.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.util.Log
-import com.alibaba.android.arouter.BuildConfig
 import com.alibaba.android.arouter.launcher.ARouter
 
 class MyApplication : Application() {
@@ -12,19 +11,22 @@ class MyApplication : Application() {
         super.onCreate()
 
         if (isDebugApp(this)) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
-            ARouter.openLog();     // 打印日志
-            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+            ARouter.openLog()     // 打印日志
+            ARouter.openDebug()  // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
-        ARouter.init(this); // 尽可能早，推荐在Application中初始化
+        ARouter.init(this) // 尽可能早，推荐在Application中初始化
     }
-    fun isDebugApp(context: Context):Boolean{
-        try {
-            val info = context.getApplicationInfo();
-            val result = (info.flags and  ApplicationInfo.FLAG_DEBUGGABLE) != 0
+    private fun isDebugApp(context: Context):Boolean{
+
+        //Log.d("test","MyApplication BuildConfig.DEBUG = ${BuildConfig.DEBUG}")
+
+        return try {
+            val info = context.applicationInfo
+            val result = (info.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
             Log.d("test","MyApplication isDebugApp = $result")
-            return result
+            result
         } catch (x:Exception) {
-            return false;
+            false
         }
     }
 }
