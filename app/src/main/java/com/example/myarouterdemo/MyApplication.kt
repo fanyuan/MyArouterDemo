6e08bf3ce7c848f8cd046badac9b2c5cacabe968
fly <lanseyoukong638@163.com>
2022-01-28 10:00:49 +0800

    判断语句日志

--- app/src/main/java/com/example/myarouterdemo/MyApplication.kt
+++ app/src/main/java/com/example/myarouterdemo/MyApplication.kt
@@ -9,21 +9,23 @@ import com.alibaba.android.arouter.launcher.ARouter
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-
+        Log.d("test","000 MyApplication BuildConfig.DEBUG = ${BuildConfig.DEBUG}")
         if (isDebugApp(this)) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
             ARouter.openLog()     // 打印日志
             ARouter.openDebug()  // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
         ARouter.init(this) // 尽可能早，推荐在Application中初始化
+
+        Log.d("test","123 MyApplication BuildConfig.DEBUG = ${BuildConfig.DEBUG}")
     }
     private fun isDebugApp(context: Context):Boolean{
 
-        Log.d("test","MyApplication BuildConfig.DEBUG = ${BuildConfig.DEBUG}")
+        //Log.d("test","MyApplication BuildConfig.DEBUG = ${BuildConfig.DEBUG}")
 
         return try {
             val info = context.applicationInfo
             val result = (info.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0
-            Log.d("test","MyApplication isDebugApp = $result")
+            Log.d("test","MyApplication isDebugApp = $result    BuildConfig.DEBUG = ${BuildConfig.DEBUG}")
             result
         } catch (x:Exception) {
             false
